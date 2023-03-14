c899c50a771e11f0e9f4593087508d539b2967b6
kempensp <kempensp@milwaukee.k12.wi.us>
2023-03-14 16:20:27 -0500

    result array for emplistall is initially set

--- expressExample/routes/emplistall.js
+++ expressExample/routes/emplistall.js
@@ -4,38 +4,41 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    // const httpRequest = require('http');
+    const httpRequest = require('http');
 
-    // const options = {
-    //   method: 'GET',
-    //   headers: {
-    //     'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJ1c2VyIjoiYmRwYW1rZWRfYWRtaW5zY290dCJ9LCJpYXQiOjE2Nzg1NTQ1MzgsImV4cCI6MTY3ODU5MDUzOH0.-WM9BMEyw9hJWj6fsyATULcvWlflQ3gP-FlpH6oRNv0',
-    //     'content-type': 'application/json'
-    //   }};
+    const options = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJ1c2VyIjoiYmRwYW1rZWRfYWRtaW5zY290dCJ9LCJpYXQiOjE2Nzg4MjY0MDQsImV4cCI6MTY3ODg2MjQwNH0.Cpyqd5EZAYhtwtNH93lcMuU_p3ftJXibUUs8CYOtZsk',
+        'content-type': 'application/json'
+      }};
     
-    // const request = httpRequest.request('http://bdpamkedev.com/api/v5/employees/?pageNo=1&pageSize=100', options, response => {
-    //   console.log('Status', response.statusCode);
-    //   console.log('Headers', response.headers);
-    //   let responseData = '';
+    const request = httpRequest.request('http://bdpamkedev.com/api/v5/employees/?pageNo=1&pageSize=100', options, response => {
+      console.log('Status', response.statusCode);
+      console.log('Headers', response.headers);
+      let responseData = '';
     
-    //   response.on('data', dataChunk => {
-    //     responseData += dataChunk;
-    //   });
-    //   response.on('end', () => {
-    //     console.log('Response: ', responseData)
-    //   });
-    // });
+      response.on('data', dataChunk => {
+        responseData += dataChunk;
+      });
+      response.on('end', () => {
+        let responseParsed=JSON.parse(responseData);
+        let responseArray=responseParsed.records;
+        console.log('Response: ', responseArray);
+        res.render('emplistall', { title: 'Products R Us Employee List' , resultarray: responseArray});
+      });
+    });
     
-    // request.on('error', error => console.log('ERROR', error));
+    request.on('error', error => console.log('ERROR', error));
     
-    // request.end();
+    request.end();
 
 
 
 
 
 
-  res.render('emplistall', { title: 'Products R Us Employee List' });
+  //res.render('emplistall', { title: 'Products R Us Employee List' , resultarray: responseArray});
 });
 
 module.exports = router;
