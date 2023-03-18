37e4ab5840f5650d81be2d9ca5f73d7591c296bd
kempensp <kempensp@milwaukee.k12.wi.us>
2023-03-18 09:45:00 -0500

    node fetch entry

--- expressExample/routes/emplistall.js
+++ expressExample/routes/emplistall.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// var api_functions = require('../public/javascripts/api_functions');
+// // set the token value when the app starts up
+// if ( typeof global.DB_token === 'undefined') {
+//     api_functions.data.db_sign_in();   
+// };
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -9,7 +15,7 @@ router.get('/', function(req, res, next) {
     const options = {
       method: 'GET',
       headers: {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJ1c2VyIjoiYmRwYW1rZWRfYWRtaW5zY290dCJ9LCJpYXQiOjE2Nzg4MjY0MDQsImV4cCI6MTY3ODg2MjQwNH0.Cpyqd5EZAYhtwtNH93lcMuU_p3ftJXibUUs8CYOtZsk',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGF0YSI6eyJ1c2VyIjoiYmRwYW1rZWRfYWRtaW5zY290dCJ9LCJpYXQiOjE2NzkxNDg5NTIsImV4cCI6MTY3OTE4NDk1Mn0._WKLH7vM7YoOL1FJlKorPvb1VWXj9l5-dUDZrLTtUjw',
         'content-type': 'application/json'
       }};
     
