f72200ec340df68c68de0857754400a8efaa4bac
kempensp <kempensp@milwaukee.k12.wi.us>
2023-04-15 16:36:29 +0000

    converted empadd to node-fetch

--- expressExample/routes/empadd.js
+++ expressExample/routes/empadd.js
@@ -9,28 +9,26 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   req.body.createdBy="bdpa_adminscott"; //Put in your own
   req.body.modifiedBy="bdpa_adminscott"; //Put in your own
-  const httpRequest = require('http'); //This may end up being https in other situations
-
-    const options = {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + global.DB_token,
-        'content-type':'application/json'
-      }};
-
-    const data=JSON.stringify(req.body);
-
-    const request = httpRequest.request('http://bdpamkedev.com/api/v5/employees', options, response => {
-      console.log('Status', response.statusCode);
-      //console.log('Headers', response.headers);
-    });
-
-request.on('error', error => console.log('ERROR', error));
-
-request.write(data);
-request.end();
-  res.render('empadd', {title:'Employee Added!'});
-});
-
+  const fetch = require('node-fetch'); //We may need to npm install this...
+  const varHttpRequest = 'https://bdpamkedev.com/api/v5/employees';
+  fetch(varHttpRequest, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + global.DB_token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(req.body)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("Message & Data ", data);
+    res.render('empadd', { title: 'Employee Add Successful', message: data.message, data: data.data });
+  })
+  .catch(error => {
+    console.error(error);
+    res.render('empadd', { title: 'Employee Add Error', message: error.message, data: error.data });
+    return "error";
+  })
+  });
 
 module.exports = router;
